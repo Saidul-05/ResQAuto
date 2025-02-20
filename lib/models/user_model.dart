2421a1d7456019a04a7a34327dc4dacd1f5bdf2e
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-21 01:19:12 +0600

    Create user_model.dart

---
+++ lib/models/user_model.dart
@@ -0,0 +1,23 @@
+// lib/models/user_model.dart
+class UserModel {
+  final String uid;
+  final String email;
+  final String name;
+
+  UserModel({required this.uid, required this.email, required this.name});
+
+  factory UserModel.fromMap(Map<String, dynamic> data, String uid) {
+    return UserModel(
+      uid: uid,
+      email: data['email'] ?? '',
+      name: data['name'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'email': email,
+      'name': name,
+    };
+  }
+}

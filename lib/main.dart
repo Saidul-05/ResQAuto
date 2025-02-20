f430d96e85fb408f269164282f314f5bfee34921
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-21 01:11:43 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'app.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const ResQAutoApp());
+}

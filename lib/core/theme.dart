b57d81f05708e338290527c092f8dd2e14a8b02c
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-21 01:13:10 +0600

    Create theme.dart

---
+++ lib/core/theme.dart
@@ -0,0 +1,16 @@
+// lib/core/theme.dart
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.light,
+    // Additional customization as needed.
+  );
+
+  static final darkTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.dark,
+    // Additional customization as needed.
+  );
+}

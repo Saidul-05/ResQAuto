d292cf6699bda6751e3dfb289c172c76748ea9e8
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-21 15:32:37 +0600

    Create app_routes.dart

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+// lib/routes/app_routes.dart
+class AppRoutes {
+  static const String home = '/';
+  static const String login = '/login';
+  static const String map = '/map';
+  // Add more routes as needed.
+}

f4bf771f135eb2c63d04fb9ac17c85b6256944f3
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-21 15:47:58 +0600

    Create firebase_config.js

---
+++ backend/config/firebase_config.js
@@ -0,0 +1,9 @@
+// backend/config/firebase_config.js
+module.exports = {
+  apiKey: 'YOUR_FIREBASE_API_KEY',
+  authDomain: 'your-app.firebaseapp.com',
+  projectId: 'your-app-id',
+  storageBucket: 'your-app.appspot.com',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
